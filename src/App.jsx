7904a477600300a770b76ffbcebf6c71553692b9
Olivia <ikajiubachidiogo@gmail.com>
2024-06-28 00:01:13 +0100

    section 3 completed,section 4 in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import { Header } from './components/Header'
+import SectionFour from './pages/SectionFour'
 import SectionOne from './pages/SectionOne'
 import SectionThree from './pages/SectionThree'
 import SectionTwo from './pages/SectionTwo'
@@ -13,6 +14,7 @@ function App() {
       <SectionOne/>
       <SectionTwo/>
       <SectionThree/>
+      <SectionFour/>
         </>
   )
 }
---
+++ src/pages/SectionFour.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import img20 from "../assets/images/img20.svg";
+import img21 from "../assets/images/img21.svg";
+import img22 from "../assets/images/img22.svg";
+import img23 from "../assets/images/img23.svg";
+
+
+
+const SectionFour = () => {
+    return (
+        <div className='flex'>
+            <div>
+                <div>
+                    <h2>
+                        Sectors we serve
+                    </h2>
+                    <p>
+                        We simplify the complexities of cross-border payments in some of the world’s most <br />demanding sectors. Learn more about who we serve and how we add value below.
+                    </p>
+                </div>
+                <div>
+                    <img src={img20} alt="" />
+                    <img src={img21} alt="" />
+                </div>
+
+            </div>
+            <div>
+                <img src={img22} alt="" />
+                <img src={img23} alt="" />
+            </div>
+
+        </div>
+    )
+}
+
+export default SectionFour
